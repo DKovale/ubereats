5d32d3b3f6179cbf36dd49982002aea81481705d
DKovale <dkov@ukr.net>
2019-07-31 22:09:58 +0300

    bitton added

---
+++ script.js
@@ -0,0 +1,16 @@
+    
+let scrollUp = document.querySelector('button.button-up');
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 210) {
+        scrollUp.style.transform = 'translateY(0px)';
+    }
+
+    else {
+        scrollUp.style.transform = 'translateY(200px)';
+    }
+});
+
+scrollUp.addEventListener('click', function () {
+    window.scrollTo(0, 0)
+});
